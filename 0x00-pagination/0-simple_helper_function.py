6eecb6c42d6125de9191bbac5df3b37686efdd00
obafisayo <sekaniisjoy@gmail.com>
2024-08-20 19:47:54 +0100

    format docstring of task 0

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,7 +2,18 @@
 '''
     index_range
 '''
-def index_range(page, page_size):
+from typing import Tuple
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """Retrieves the index from a given page and page size
+    
+    Keyword arguments:
+    page -- the page to be located
+    page_size -- the size of the page
+    Return: a tuple of the start index and end index.
+    """
+    
     start_index = (page - 1) * page_size
     end_index = start_index + page_size
     return (start_index, end_index)
