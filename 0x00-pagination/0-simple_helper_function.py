b48241168f060490047dd95aed9f2551642ba735
obafisayo <sekaniisjoy@gmail.com>
2024-08-20 19:50:28 +0100

    ok

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python3
-'''
-    index_range
-'''
+"""Pagination helper function.
+"""
 from typing import Tuple
 
 
-def index_range(page, page_size) -> Tuple[int, int]:
-    """Retrieves the index from a given page and page size
-    
-    Keyword arguments:
-    page -- the page to be located
-    page_size -- the size of the page
-    Return: a tuple of the start index and end index.
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Retrieves the index range from a given page and page size.
     """
-    
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
-    return (start_index, end_index)
+    start = (page - 1) * page_size
+    end = start + page_size
+    return (start, end)
--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+'''
+    index_range
+'''
+from typing import Tuple
+
+
+def index_range(page, page_size) -> Tuple[int, int]:
+    """Retrieves the index from a given page and page size
+    
+    Keyword arguments:
+    page -- the page to be located
+    page_size -- the size of the page
+    Return: a tuple of the start index and end index.
+    """
+    
