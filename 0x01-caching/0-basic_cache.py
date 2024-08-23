094366604cfa4ca1abe59ba4d99ac6cc3cde0ec7
obafisayo <sekaniisjoy@gmail.com>
2024-08-23 01:27:01 +0100

    added docstring for base cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -9,11 +9,15 @@ class BasicCache(BaseCaching):
     A basic caching system that inherits from the superclass basecaching
     """
     def put(self, key, item):
+        """Adds an item to the cache
+        """
         if key is None or item is None:
             return
         self.cache_data[key] = item
 
     def get(self, key):
+        """Retrieves an item from the cache by key
+        """
         if key is None:
             return
         if key not in self.cache_data:
